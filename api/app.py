d04c710dc46b0d2859e621b337791706794909cb
Brian Mak <bm323@imperial.ac.uk>
2023-10-25 12:07:05 +0100

    student_id

--- api/app.py
+++ api/app.py
@@ -14,7 +14,7 @@ def submit():
     input_studentid = request.form.get("studentid")
     output_age = int(input_age) + 25
     return render_template("hello.html", name=input_name,
-                           age=input_age, another_age=output_age, 
+                           age=input_age, another_age=output_age,
                            student_id=input_studentid)
 
 
