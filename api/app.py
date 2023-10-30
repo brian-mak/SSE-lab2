6b4357d4b2d0db66cb55f6a4219d5b4ec5f9081e
Brian Mak <bm323@imperial.ac.uk>
2023-10-30 16:16:10 +0000

    add multiply

--- api/app.py
+++ api/app.py
@@ -30,18 +30,30 @@ def process_query(query):
         return "Whatever"
 
     if ("What is " in query and "plus" in query):
-        number_1_start = query.index("is ") + len("is ")
-        number_1_end = query.index("plus")
-        number_1 = int(query[number_1_start:number_1_end])
-
-        number_2_start = query.index("plus ") + len("plus ")
-        number_2_end = query.index("?")
-        number_2 = int(query[number_2_start:number_2_end])
-
-        return str(number_1 + number_2)
+        return str(plus_two_numbers(query))
 
     if ("Which of the following numbers is the largest:") in query:
         return find_largest_number(query)
+
+    if ("What is " in query and "multiplied by " in query):
+        return str(multiply_two_numbers(query))
+
+
+103
+
+    return (number_1 + number_2)
+
+
+def multiply_two_numbers(query):
+    number_1_start = query.index("is ") + len("is ")
+    number_1_end = query.index("multiplied")
+    number_1 = int(query[number_1_start:number_1_end])
+
+    number_2_start = query.index("multiplied by ") + len("multiplied by ")
+    number_2_end = query.index("?")
+    number_2 = int(query[number_2_start:number_2_end])
+
+    return (number_1 * number_2)
 
 
 def find_largest_number(query):
