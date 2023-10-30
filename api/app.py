87eac08287d392759d856938f00a26e88d681a7a
Brian Mak <bm323@imperial.ac.uk>
2023-10-30 15:42:53 +0000

    add plus

--- api/app.py
+++ api/app.py
@@ -30,7 +30,7 @@ def process_query(query):
 
     if ("What is " in query and "plus" in query):
         number_1_start = query.index("is ") + len("is ")
-        number_1_end = query.index("plus") - 1 
+        number_1_end = query.index("plus") - 1
         number_1 = int(query[number_1_start:number_1_end])
 
         number_2_start = query.index("plus ") + len("plus ")
